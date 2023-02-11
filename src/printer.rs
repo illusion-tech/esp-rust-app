e06a52be254079111309057837c4f2e129536ff9
Wang Zishi <ynh.2@outlook.com>
2023-02-12 01:39:06 +0800

    feat: 定义 printer 模块

---
+++ src/printer.rs
@@ -0,0 +1,29 @@
+#[macro_export]
+macro_rules! println {
+    ($($arg:tt)*) => {{
+        use core::fmt::Write;
+        use crate::printer::Printer;
+        writeln!(Printer, $($arg)*).ok();
+    }};
+}
+
+const UART_TX_ONE_CHAR: usize = 0x4000_0068;
+pub struct Printer;
+
+impl Printer {
+    fn write_bytes(&mut self, bytes: &[u8]) {
+        for &byte in bytes {
+            unsafe {
+                type T = unsafe extern "C" fn(u8) -> i32;
+                core::mem::transmute::<usize, T>(UART_TX_ONE_CHAR)(byte);
+            }
+        }
+    }
+}
+
+impl core::fmt::Write for Printer {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        Printer.write_bytes(s.as_bytes());
+        Ok(())
+    }
+}
