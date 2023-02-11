de0994090a9254f7972aecfff2556aa9e0aeadea
Wang Zishi <ynh.2@outlook.com>
2023-02-11 23:57:12 +0800

    feat: 使用 uart 实现输出日志 println

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,57 @@ use esp32c3_hal::{
     clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc,
 };
 use esp_backtrace as _;
+use log::{set_logger_racy, set_max_level, LevelFilter, Log, Metadata, Record};
+
+const UART_TX_ONE_CHAR: usize = 0x4000_0068;
+struct Printer;
+
+impl Printer {
+    fn write_bytes(&mut self, bytes: &[u8]) {
+        for &byte in bytes {
+            unsafe {
+                type T = unsafe extern "C" fn(u8) -> i32;
+                core::mem::transmute::<usize, T>(UART_TX_ONE_CHAR)(byte);
+            }
+        }
+    }
+}
+
+impl core::fmt::Write for Printer {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        Printer.write_bytes(s.as_bytes());
+        Ok(())
+    }
+}
+macro_rules! println {
+    ($($arg:tt)*) => {{
+        use core::fmt::Write;
+        writeln!(Printer, $($arg)*).ok();
+    }};
+}
+
+struct EspLogger;
+
+impl Log for EspLogger {
+    fn enabled(&self, _metadata: &Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &Record) {
+        println!("{} - {}", record.level(), record.args());
+    }
+
+    fn flush(&self) {
+        todo!()
+    }
+}
+
+fn init_logger(level: LevelFilter) {
+    unsafe {
+        set_logger_racy(&EspLogger).unwrap();
+        set_max_level(level);
+    }
+}
 
 #[riscv_rt::entry]
 fn main() -> ! {
