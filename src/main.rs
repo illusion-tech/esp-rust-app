fb7668865bf47a76439a11d3d73eb919418d1344
Wang Zishi <ynh.2@outlook.com>
2023-02-12 16:24:44 +0800

    feat: 可编程 led 脉冲控制

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@
 #![no_main]
 mod printer;
 use esp32c3_hal::{
-    clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Delay, Rtc, IO,
+    clock::ClockControl, peripherals::Peripherals, prelude::*, pulse_control::RepeatMode,
+    timer::TimerGroup, Delay, Rtc, IO,
 };
 use esp_backtrace as _;
 
 #[riscv_rt::entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
-    let system = peripherals.SYSTEM.split();
+    let mut system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
     // Disable the RTC and TIMG watchdog timers
@@ -29,6 +30,35 @@ fn main() -> ! {
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let button = io.pins.gpio9.into_pull_up_input();
     let mut delay = Delay::new(&clocks);
+
+    // let b = system.;
+    let pulse = esp32c3_hal::PulseControl::new(
+        peripherals.RMT,
+        &mut system.peripheral_clock_control,
+        esp32c3_hal::pulse_control::ClockSource::APB,
+        0,
+        0,
+        0,
+    )
+    .unwrap();
+
+    let mut channel = pulse.channel0;
+
+    channel
+        .set_idle_output_level(false)
+        .set_carrier_modulation(false)
+        .set_channel_divider(1)
+        .set_idle_output(true);
+
+    let mut led = channel.assign_pin(io.pins.gpio8);
+
+    // let mut rmt_buffer = [0u32; 25];
+    // rmt_buffer
+
+    match led.send_pulse_sequence_raw(RepeatMode::SingleShot, &[0u32; 25]) {
+        Ok(_) => println!("Success!"),
+        Err(e) => println!("Error: {:?}", e),
+    }
 
     loop {
         if button.is_high().unwrap() {
