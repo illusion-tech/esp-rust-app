111752a1d759fa5c486a71e883aab8b86167dbd9
Wang Zishi <ynh.2@outlook.com>
2023-02-18 04:11:59 +0800

    feat: server 解析并响应 modbus 请求

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
+
 mod printer;
 use embassy_executor::Executor;
 use embassy_time::{Duration, Timer};
 use embedded_hal_async::digital::Wait;
 use esp32c3_hal::{
     embassy,
-    gpio::{Floating, Gpio10, Gpio9, Input, Output, PullDown, PushPull},
+    gpio::{Gpio10, Gpio9, Input, Output, PullDown, PushPull},
     peripherals::UART1,
     prelude::*,
     Uart,
@@ -21,80 +22,97 @@ use esp_backtrace as _;
 use static_cell::StaticCell;
 
 #[embassy_executor::task]
-async fn run1() {
+async fn run() {
     loop {
-        println!("Hello world from embassy using esp-hal-async!");
-        Timer::after(Duration::from_millis(1_000)).await;
+        print!(".");
+        Timer::after(Duration::from_secs(1)).await;
     }
 }
 
 #[embassy_executor::task]
-async fn run2() {
-    #[cfg(feature = "server")]
+async fn server(mut serial: Uart<'static, UART1>) {
+    fn test(buf: &rmodbus::ModbusFrameBuf) {
+        let mut mem = fixedvec::alloc_stack!([u8; 256]);
+        let mut response = fixedvec::FixedVec::new(&mut mem);
+        let mut frame =
+            rmodbus::server::ModbusFrame::new(1, buf, rmodbus::ModbusProto::Rtu, &mut response);
+
+        if frame.parse().is_err() {
+            println!("server error");
+            return;
+        }
+        if frame.processing_required {
+            println!("processing required");
+            
+            // let result = match frame.readonly {
+            //     true => frame.process_read(&CONTEXT.read().unwrap()),
+            //     false => frame.process_write(&mut CONTEXT.write().unwrap()),
+            // };
+            // if result.is_err() {
+            //     println!("frame processing error");
+            //     return;
+            // }
+        }
+
+        if frame.response_required {
+            println!("response required");
+            frame.finalize_response().unwrap();
+            println!("{:x?}", response);
+            // if stream.write(response.as_slice()).is_err() {
+            //     return;
+            // }
+        } else {
+            println!("no response required");
+        }
+    }
+
+    let mut mem = fixedvec::alloc_stack!([u8; 256]);
+    let mut ptr = 0;
+    let mut idle = true;
+    let mut delay = 0u8;
     loop {
-        println!("Bing!");
-        Timer::after(Duration::from_millis(5_000)).await;
+        if idle {
+            print!("x");
+            Timer::after(Duration::from_millis(100)).await;
+        }
+
+        while let Ok(byte) = serial.read() {
+            idle = false;
+            println!("{byte:02x}");
+            mem[ptr] = byte;
+            ptr += 1;
+            delay = 0;
+            Timer::after(Duration::from_micros(100)).await;
+        }
+
+        if idle {
+            continue;
+        }
+
+        delay += 1;
+
+        if delay > 10 {
+            if ptr != 0 {
+                println!("Received: {:?}, {}, {}", mem, delay, ptr);
+                test(&mem);
+                mem.fill(0);
+                ptr = 0;
+            }
+
+            idle = true;
+            delay = 0;
+        }
     }
 }
 
 #[embassy_executor::task]
-async fn run3(
+async fn receiver(
     mut button: Gpio9<Input<PullDown>>,
     mut serial: Uart<'static, UART1>,
     mut rts: Gpio10<Output<PushPull>>,
 ) {
-    #[cfg(feature = "server")]
-    let mut mem = fixedvec::alloc_stack!([u8; 256]);
-    #[cfg(feature = "server")]
-    loop {
-        // let byte = serial.read().unwrap();
-        // let r = );
-        let mut ticks = 0u8;
-        let mut idx = 0;
-        loop {
-            while let Ok(byte) = serial.read() {
-                println!("{byte:02x}");
-                mem[idx] = byte;
-                idx += 1;
-                ticks = 0;
-                Timer::after(Duration::from_micros(100)).await;
-            }
-
-            if idx != 0 {
-                ticks += 1;
-            }
-
-            if ticks > 10 {
-                if idx != 0 {
-                    break;
-                }
-                
-                ticks = 0;
-            }
-        }
-
-        println!("Received: {:?}, {}, {}", mem, ticks, idx);
-
-        mem.fill(0);
-        idx = 0;
-    }
-
-    // match serial.read() {
-    //     Ok(byte) => {
-    //         println!("{byte:02x}");
-    //         ticks = 0;
-    //     }
-    //     Err(e) => {
-    //         ticks += 1;
-    //         println!("Error: {:?}", e);
-    //     }
-    // }
-    // print!("{byte:02x} ");
-    // }
-    #[cfg(not(feature = "server"))]
     loop {
         button.wait_for_rising_edge().await.unwrap();
-        println!("Ping!");
 
         let mut client = rmodbus::client::ModbusRequest::new(1, rmodbus::ModbusProto::Rtu);
 
@@ -109,7 +127,6 @@ async fn run3(
         Timer::after(Duration::from_micros(10)).await;
         serial.write_bytes(request.as_slice()).unwrap();
         println!("{} bytes written", request.len());
-        // serial.write(b'.').unwrap();
 
         // let d = <usize as TryInto<u64>>::try_into(request.len()).unwrap() * 2;
         // Timer::after(Duration::from_micros(d)).await;
@@ -173,10 +190,10 @@ fn main() -> ! {
     let executor = EXECUTOR.init(Executor::new());
 
     executor.run(|spawner| {
-        spawner.spawn(run1()).ok();
-        spawner.spawn(run2()).ok();
-        spawner.spawn(run3(button, serial1, rts)).ok();
-
-        // serial1.read();
+        spawner.spawn(run()).ok();
+        #[cfg(feature = "server")]
+        spawner.spawn(server(serial1)).ok();
+        #[cfg(feature = "client")]
+        spawner.spawn(receiver(button, serial1, rts)).ok();
     });
 }
