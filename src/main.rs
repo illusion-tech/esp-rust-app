c5fb511c15f3caf6bc0312e18cdf4879819339c6
Wang Zishi <ynh.2@outlook.com>
2023-02-17 18:08:10 +0800

    feat: 引入 embassy 框架，并处理串口数据读取

--- src/main.rs
+++ src/main.rs
@@ -1,202 +1,182 @@
+//! embassy hello world
+//!
+//! This is an example of running the embassy executor with multiple tasks
+//! concurrently.
+
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 mod printer;
-
-use core::{cell::RefCell, fmt::Write};
-// use ;
-use critical_section::Mutex;
+use embassy_executor::Executor;
+use embassy_time::{Duration, Timer};
+use embedded_hal_async::digital::Wait;
 use esp32c3_hal::{
-    clock::ClockControl,
-    gpio::{Event, Gpio10, Gpio9, Input, Output, PullDown, PushPull, IO},
-    interrupt::{self, CpuInterrupt, InterruptKind, Priority},
-    peripherals::{Interrupt, Peripherals, UART1},
+    embassy,
+    gpio::{Floating, Gpio10, Gpio9, Input, Output, PullDown, PushPull},
+    peripherals::UART1,
     prelude::*,
-    riscv,
-    timer::TimerGroup,
-    uart::{
-        config::{AtCmdConfig, Config, DataBits, Parity, StopBits},
-        AllPins, TxRxPins,
-    },
-    Cpu, Delay, Rtc, Uart,
+    Uart,
 };
 use esp_backtrace as _;
+use static_cell::StaticCell;
 
-// use rmodbus
+#[embassy_executor::task]
+async fn run1() {
+    loop {
+        println!("Hello world from embassy using esp-hal-async!");
+        Timer::after(Duration::from_millis(1_000)).await;
+    }
+}
 
-static DELAY: Mutex<RefCell<Option<Delay>>> = Mutex::new(RefCell::new(None));
+#[embassy_executor::task]
+async fn run2() {
+    #[cfg(feature = "server")]
+    loop {
+        println!("Bing!");
+        Timer::after(Duration::from_millis(5_000)).await;
+    }
+}
 
-static BUTTON: Mutex<RefCell<Option<Gpio9<Input<PullDown>>>>> = Mutex::new(RefCell::new(None));
-static GPIO10: Mutex<RefCell<Option<Gpio10<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
-static SERIAL1: Mutex<RefCell<Option<Uart<UART1>>>> = Mutex::new(RefCell::new(None));
+#[embassy_executor::task]
+async fn run3(
+    mut button: Gpio9<Input<PullDown>>,
+    mut serial: Uart<'static, UART1>,
+    mut rts: Gpio10<Output<PushPull>>,
+) {
+    #[cfg(feature = "server")]
+    let mut mem = fixedvec::alloc_stack!([u8; 256]);
+    #[cfg(feature = "server")]
+    loop {
+        // let byte = serial.read().unwrap();
+        // let r = );
+        let mut ticks = 0u8;
+        let mut idx = 0;
+        loop {
+            while let Ok(byte) = serial.read() {
+                println!("{byte:02x}");
+                mem[idx] = byte;
+                idx += 1;
+                ticks = 0;
+                Timer::after(Duration::from_micros(100)).await;
+            }
+
+            if idx != 0 {
+                ticks += 1;
+            }
+
+            if ticks > 10 {
+                if idx != 0 {
+                    break;
+                }
+                
+                ticks = 0;
+            }
+        }
+
+        println!("Received: {:?}, {}, {}", mem, ticks, idx);
+
+        mem.fill(0);
+        idx = 0;
+    }
+
+    // match serial.read() {
+    //     Ok(byte) => {
+    //         println!("{byte:02x}");
+    //         ticks = 0;
+    //     }
+    //     Err(e) => {
+    //         ticks += 1;
+    //         println!("Error: {:?}", e);
+    //     }
+    // }
+    // print!("{byte:02x} ");
+    // }
+    #[cfg(not(feature = "server"))]
+    loop {
+        button.wait_for_rising_edge().await.unwrap();
+        println!("Ping!");
+
+        let mut client = rmodbus::client::ModbusRequest::new(1, rmodbus::ModbusProto::Rtu);
+
+        let mut mem = fixedvec::alloc_stack!([u8; 256]);
+        let mut request = fixedvec::FixedVec::new(&mut mem);
+        client.generate_set_coil(100, true, &mut request).unwrap();
+
+        println!("Request: {:?}", request);
+
+        rts.set_high().unwrap();
+
+        Timer::after(Duration::from_micros(10)).await;
+        serial.write_bytes(request.as_slice()).unwrap();
+        println!("{} bytes written", request.len());
+        // serial.write(b'.').unwrap();
+
+        // let d = <usize as TryInto<u64>>::try_into(request.len()).unwrap() * 2;
+        // Timer::after(Duration::from_micros(d)).await;
+        rts.set_low().unwrap();
+    }
+}
+
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take();
+    println!("Init!");
+    let peripherals = esp32c3_hal::peripherals::Peripherals::take();
     let system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let clocks = esp32c3_hal::clock::ClockControl::boot_defaults(system.clock_control).freeze();
+    let sys_timer = esp32c3_hal::systimer::SystemTimer::new(peripherals.SYSTIMER);
 
-    // Disable the watchdog timers. For the ESP32-C3, this includes the Super WDT,
-    // the RTC WDT, and the TIMG WDTs.
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    embassy::init(&clocks, sys_timer);
+
+    let mut rtc = esp32c3_hal::Rtc::new(peripherals.RTC_CNTL);
+    let timer_group0 = esp32c3_hal::timer::TimerGroup::new(peripherals.TIMG0, &clocks);
     let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+    let timer_group1 = esp32c3_hal::timer::TimerGroup::new(peripherals.TIMG1, &clocks);
     let mut wdt1 = timer_group1.wdt;
 
+    // Disable watchdog timers
     rtc.swd.disable();
     rtc.rwdt.disable();
     wdt0.disable();
     wdt1.disable();
 
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-
-    // 设置 GPIO9（板载 boot 按钮）为输入
-    let mut button = io.pins.gpio9.into_pull_down_input();
-    // 设置 GPIO10 为输出控制 MAX3485 收发状态
-    let mut gpio10 = io.pins.gpio10.into_push_pull_output();
-    // 设置 UART1 控制器，并使用了 GPIO0 和 GPIO1 作为 TX 和 RX
-    // let pins = AllPins::new(
-    //     io.pins.gpio0.into_push_pull_output(),
-    //     io.pins.gpio1.into_pull_up_input(),
-    //     io.pins.gpio10.into_push_pull_output(),
-    //     io.pins.gpio18.into_push_pull_output(),
-    // );
-    let pins = TxRxPins::new_tx_rx(
-        io.pins.gpio0.into_push_pull_output(),
-        io.pins.gpio1.into_pull_up_input(),
-    );
-    let config = Config {
+    let io = esp32c3_hal::IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let button = io.pins.gpio9.into_pull_down_input();
+    let mut rts = io.pins.gpio10.into_push_pull_output();
+    let config = esp32c3_hal::uart::config::Config {
         baudrate: 115_200,
-        data_bits: DataBits::DataBits8,
-        parity: Parity::ParityEven,
-        stop_bits: StopBits::STOP1,
+        data_bits: esp32c3_hal::uart::config::DataBits::DataBits8,
+        parity: esp32c3_hal::uart::config::Parity::ParityEven,
+        stop_bits: esp32c3_hal::uart::config::StopBits::STOP1,
     };
-    let mut serial1 = Uart::new_with_config(peripherals.UART1, Some(config), Some(pins), &clocks);
+    // let pins = esp32c3_hal::uart::AllPins::<_, _, Gpio9<Input<Floating>>, _> {
+    //     tx: Some(io.pins.gpio0.into_push_pull_output().into_ref()),
+    //     rx: Some(io.pins.gpio1.into_floating_input().into_ref()),
+    //     rts: Some(io.pins.gpio10.into_push_pull_output().into_ref()),
+    //     cts: None,
+    // };
+    let pins = esp32c3_hal::uart::TxRxPins::new_tx_rx(
+        io.pins.gpio0.into_push_pull_output(),
+        io.pins.gpio1.into_floating_input(),
+    );
+    let serial1 =
+        esp32c3_hal::Uart::new_with_config(peripherals.UART1, Some(config), Some(pins), &clocks);
 
-    button.listen(Event::FallingEdge);
-    gpio10.set_low().unwrap();
+    rts.set_low().unwrap();
+    esp32c3_hal::interrupt::enable(
+        esp32c3_hal::peripherals::Interrupt::GPIO,
+        esp32c3_hal::interrupt::Priority::Priority3,
+    )
+    .unwrap();
 
-    serial1.set_at_cmd(AtCmdConfig::new(None, None, None, b'#', None));
-    serial1.set_rx_fifo_full_threshold(64);
-    serial1.listen_at_cmd();
-    serial1.listen_rx_fifo_full();
-    serial1.listen_tx_done();
+    let executor = EXECUTOR.init(Executor::new());
 
-    #[cfg(feature = "client")]
-    {
-        critical_section::with(|cs| BUTTON.borrow_ref_mut(cs).replace(button));
-        critical_section::with(|cs| GPIO10.borrow_ref_mut(cs).replace(gpio10));
-        critical_section::with(|cs| SERIAL1.borrow_ref_mut(cs).replace(serial1));
+    executor.run(|spawner| {
+        spawner.spawn(run1()).ok();
+        spawner.spawn(run2()).ok();
+        spawner.spawn(run3(button, serial1, rts)).ok();
 
-        interrupt::enable(Interrupt::GPIO, Priority::Priority3).unwrap();
-        interrupt::enable(Interrupt::UART1, Priority::Priority1).unwrap();
-        interrupt::set_kind(Cpu::ProCpu, CpuInterrupt::Interrupt1, InterruptKind::Edge);
-
-        unsafe {
-            riscv::interrupt::enable();
-        }
-    }
-
-    let delay = Delay::new(&clocks);
-
-    critical_section::with(|cs| DELAY.borrow_ref_mut(cs).replace(delay));
-
-    #[allow(clippy::empty_loop)]
-    loop {
-        #[cfg(feature = "server")]
-        {
-            let byte = nb::block!(serial1.read()).unwrap();
-            print!("{byte:02x} ");
-        }
-
-        // Delay::new(&clocks).delay_ms(500u32);
-    }
-}
-
-#[cfg(feature = "client")]
-#[interrupt]
-fn GPIO() {
-    critical_section::with(|cs| {
-        println!("GPIO 中断");
-
-        let mut serial1 = SERIAL1.borrow_ref_mut(cs);
-        let serial1 = serial1.as_mut().unwrap();
-        let mut gpio10 = GPIO10.borrow_ref_mut(cs);
-        let gpio10 = gpio10.as_mut().unwrap();
-
-        // 设置高电平输出使 MAX3485 进入发送状态
-        gpio10.set_high().unwrap();
-
-        // 通过串口发送数据
-        // write!(serial1, "#Hello World!").ok();
-
-        let mut mdb_req = rmodbus::client::ModbusRequest::new(1, rmodbus::ModbusProto::Rtu);
-        let mut space = fixedvec::alloc_stack!([u8; 256]);
-        let mut request = fixedvec::FixedVec::new(&mut space);
-        mdb_req.generate_set_coil(0, true, &mut request).unwrap();
-        // mdb_req
-        //     .generate_set_holdings_string(
-        //         0,
-        //         "Hello World from ESP32C3 using Rust over RS483 using Modbus protocol!\r\n",
-        //         &mut request,
-        //     )
-        //     .unwrap();
-
-        print!("{:?}", request);
-        serial1.write_bytes(request.as_slice()).ok();
-
-        let mut delay = DELAY.borrow_ref_mut(cs);
-        let delay = delay.as_mut().unwrap();
-
-        nb::block!(serial1.flush()).ok();
-        delay.delay_ms(request.len() as u32);
-
-        BUTTON
-            .borrow_ref_mut(cs)
-            .as_mut()
-            .unwrap()
-            .clear_interrupt();
-
-        // 设置低电平使 MAX3485 恢复接受状态
-        gpio10.set_low().unwrap();
-    });
-}
-
-#[cfg(feature = "client")]
-#[interrupt]
-fn UART1() {
-    critical_section::with(|cs| {
-        println!("\r\nUART1 中断");
-
-        let mut serial1 = SERIAL1.borrow_ref_mut(cs);
-        let serial1 = serial1.as_mut().unwrap();
-
-        if serial1.tx_done_interrupt_set() {
-            println!("TX-DONE 中断");
-            serial1.reset_tx_done_interrupt();
-
-            return;
-        }
-
-        println!(
-            "中断类型 AT-CMD: {} RX-FIFO-FULL: {}",
-            serial1.at_cmd_interrupt_set(),
-            serial1.rx_fifo_full_interrupt_set()
-        );
-
-        // let mut buf: rmodbus::ModbusFrameBuf = [0; 256];
-
-        // 从串口中读取数据直至缓冲区为空
-        let mut cnt = 0;
-        // let x = nb::block!(serial1.read());
-        while let Ok(_byte) = nb::block!(serial1.read()) {
-            cnt += 1;
-            print!("{}", _byte as char);
-        }
-
-        println!("\r\n读取 {} 字节", cnt);
-
-        serial1.reset_at_cmd_interrupt();
-        serial1.reset_rx_fifo_full_interrupt();
+        // serial1.read();
     });
 }
